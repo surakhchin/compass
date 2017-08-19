4584e5a4294c99769d7c81053b7601735e3bf8cf
Serge <urakhchin@gmail.com>
2017-08-18 21:34:43 -0700

    shot in the dark test

--- app.js
+++ app.js
@@ -194,9 +194,9 @@
 
       // apply rotation to compass rose
       if (typeof rose.style.transform !== "undefined") {
-        rose.style.transform = "rotateZ(" + positionCurrent.hng + "deg)";
+        rose.style.transform = "rotateZ(" - positionCurrent.hng + "deg)";
       } else if (typeof rose.style.webkitTransform !== "undefined") {
-        rose.style.webkitTransform = "rotateZ(" + positionCurrent.hng + "deg)";
+        rose.style.webkitTransform = "rotateZ(" - positionCurrent.hng + "deg)";
       }
     } else {
       // device can't show heading
